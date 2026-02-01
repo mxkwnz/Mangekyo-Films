49f8943771dbd3ef8e7bfd9270ce7579958b6d54
Muhammedali <241773@astanait.edu.kz>
2026-02-01 12:47:25 +0500

    implement movie repository and CRUD endpoints

--- internal/models/movie.go
+++ internal/models/movie.go
@@ -1,1 +1,14 @@
 package models
+
+import "time"
+
+type Movie struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Duration    int       `json:"duration"`
+	Description string    `json:"description"`
+	PosterURL   string    `json:"poster_url"`
+	Authors     []string  `json:"authors"`
+	Rating      float64   `json:"rating"`
+	CreatedAt   time.Time `json:"created_at"`
+}
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,1 +1,12 @@
 package routes
+
+import (
+	"cinema-system/internal/handlers"
+	"net/http"
+)
+
+func RegisterRoutes(
+	movieHandler *handlers.MovieHandler,
+) {
+	http.HandleFunc("/movies", movieHandler.HandleMovies)
+}
