1e38c4851b92623e21ee621e03945ff747564c75
alixx21 <xxsixei@gmail.com>
2026-02-01 14:42:41 +0500

    Register ticket routes in router

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -1,12 +1,15 @@
 package routes
 
 import (
-	"cinema-system/internal/handlers"
 	"net/http"
+
+	"cinema-system/internal/handlers"
 )
 
 func RegisterRoutes(
 	movieHandler *handlers.MovieHandler,
+	ticketHandler *handlers.TicketHandler,
 ) {
 	http.HandleFunc("/movies", movieHandler.HandleMovies)
+	http.HandleFunc("/tickets", ticketHandler.HandleTickets)
 }
