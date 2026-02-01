dbca16a407dbfbe2877848a6f331edf08bfc1c1a
alixx21 <xxsixei@gmail.com>
2026-02-01 14:30:52 +0500

    Implement booking worker using goroutine and channel

--- internal/services/booking_worker.go
+++ internal/services/booking_worker.go
@@ -1,1 +1,21 @@
 package services
+
+import "log"
+
+type BookingWorker struct {
+	Queue chan string
+}
+
+func NewBookingWorker() *BookingWorker {
+	return &BookingWorker{
+		Queue: make(chan string, 10),
+	}
+}
+
+func (w *BookingWorker) Start() {
+	go func() {
+		for msg := range w.Queue {
+			log.Println("Booking processed:", msg)
+		}
+	}()
+}
