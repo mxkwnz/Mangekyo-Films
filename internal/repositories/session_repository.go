0938f6ecda7042fc4b7a7d7186b48a89843d66f6
Muhammedali <241773@astanait.edu.kz>
2026-02-08 19:09:25 +0500

    create movie repository with CRUD operations and implement session repository with time-based queries

--- internal/repositories/session_repository.go
+++ internal/repositories/session_repository.go
@@ -2,40 +2,75 @@ package repositories
 
 import (
 	"cinema-system/internal/models"
-	"sync"
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"time"
 )
 
-type SessionRepo struct {
-	mu     sync.RWMutex
-	data   map[int]models.Session
-	nextID int
+type SessionRepository struct {
+	collection *mongo.Collection
 }
 
-func NewSessionRepo() *SessionRepo {
-	return &SessionRepo{
-		data:   make(map[int]models.Session),
-		nextID: 1,
+func NewSessionRepository(db *mongo.Database) *SessionRepository {
+	return &SessionRepository{
+		collection: db.Collection("sessions"),
 	}
 }
 
-func (r *SessionRepo) Create(session models.Session) models.Session {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-
-	session.ID = r.nextID
-	r.nextID++
-
-	r.data[session.ID] = session
-	return session
+func (r *SessionRepository) Create(ctx context.Context, session *models.Session) error {
+	result, err := r.collection.InsertOne(ctx, session)
+	if err != nil {
+		return err
+	}
+	session.ID = result.InsertedID.(primitive.ObjectID)
+	return nil
 }
 
-func (r *SessionRepo) GetAll() []models.Session {
-	r.mu.RLock()
-	defer r.mu.RUnlock()
-
-	list := []models.Session{}
-	for _, s := range r.data {
-		list = append(list, s)
+func (r *SessionRepository) FindByID(ctx context.Context, id primitive.ObjectID) (*models.Session, error) {
+	var session models.Session
+	err := r.collection.FindOne(ctx, bson.M{"_id": id}).Decode(&session)
+	if err != nil {
+		return nil, err
 	}
-	return list
+	return &session, nil
+}
+
+func (r *SessionRepository) GetByMovie(ctx context.Context, movieID primitive.ObjectID) ([]models.Session, error) {
+	cursor, err := r.collection.Find(ctx, bson.M{
+		"movie_id":   movieID,
+		"start_time": bson.M{"$gte": time.Now()},
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var sessions []models.Session
+	if err = cursor.All(ctx, &sessions); err != nil {
+		return nil, err
+	}
+	return sessions, nil
+}
+
+func (r *SessionRepository) GetUpcoming(ctx context.Context) ([]models.Session, error) {
+	cursor, err := r.collection.Find(ctx, bson.M{
+		"start_time": bson.M{"$gte": time.Now()},
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var sessions []models.Session
+	if err = cursor.All(ctx, &sessions); err != nil {
+		return nil, err
+	}
+	return sessions, nil
+}
+
+func (r *SessionRepository) Delete(ctx context.Context, id primitive.ObjectID) error {
+	_, err := r.collection.DeleteOne(ctx, bson.M{"_id": id})
+	return err
 }
